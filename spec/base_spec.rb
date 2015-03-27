d137d3c54dd86e95dc3f1852ee3b62270bb83d40
Simon Kaczor <skaczor@prognosoft.biz>
2015-03-27 09:39:55 -0400

    Convert specs to RSpec 2.14.8 syntax with Transpec
    This conversion is done by Transpec 3.1.0 with the following command:
        transpec -f
    
    * 43 conversions
        from: obj.should
          to: expect(obj).to
    
    * 26 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 13 conversions
        from: == expected
          to: eq(expected)
    
    * 5 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 4 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 2 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 conversion
        from: mock('something')
          to: double('something')
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -15,67 +15,67 @@ describe Throttling do
     { :check_ip => '127.0.0.1', :check_user_id => 123 }.each do |check_method, valid_value|
       describe check_method do
         it 'should return true for nil check_values' do
-          @t.send(check_method, nil).should be_true
+          expect(@t.send(check_method, nil)).to be_true
         end
 
         it 'should return true if no limit specified in configs' do
           Throttling.limits['foo']['limit'] = nil
-          @storage.should_receive(:fetch).and_return(1000)
-          @t.send(check_method, valid_value).should be_true
+          expect(@storage).to receive(:fetch).and_return(1000)
+          expect(@t.send(check_method, valid_value)).to be_true
         end
 
         it 'should return false if limit is 0' do
           Throttling.limits['foo']['limit'] = 0
-          @storage.should_receive(:fetch).and_return(0)
-          @t.send(check_method, valid_value).should be_false
+          expect(@storage).to receive(:fetch).and_return(0)
+          expect(@t.send(check_method, valid_value)).to be_false
         end
 
         it 'should raise an exception if no period specified in configs' do
           Throttling.limits['foo']['period'] = nil
-          lambda { @t.send(check_method, valid_value) }.should raise_error(ArgumentError)
+          expect { @t.send(check_method, valid_value) }.to raise_error(ArgumentError)
         end
 
         it 'should raise an exception if invalid period specified in configs' do
           Throttling.limits['foo']['period'] = -1
-          lambda { @t.send(check_method, valid_value) }.should raise_error(ArgumentError)
+          expect { @t.send(check_method, valid_value) }.to raise_error(ArgumentError)
 
           Throttling.limits['foo']['period'] = 'foo'
-          lambda { @t.send(check_method, valid_value) }.should raise_error(ArgumentError)
+          expect { @t.send(check_method, valid_value) }.to raise_error(ArgumentError)
         end
 
         it 'should return true if throttling limit is not passed' do
-          @storage.should_receive(:fetch).and_return(1)
-          @t.send(check_method, valid_value).should be_true
+          expect(@storage).to receive(:fetch).and_return(1)
+          expect(@t.send(check_method, valid_value)).to be_true
         end
 
         it 'should return false if throttling limit is passed' do
-          @storage.should_receive(:fetch).and_return(Throttling.limits['foo']['limit'] + 1)
-          @t.send(check_method, valid_value).should be_false
+          expect(@storage).to receive(:fetch).and_return(Throttling.limits['foo']['limit'] + 1)
+          expect(@t.send(check_method, valid_value)).to be_false
         end
 
         context 'around limit' do
           it 'should increase hit counter when values equals to limit - 1' do
-            @storage.should_receive(:fetch).and_return(Throttling.limits['foo']['limit'] - 1)
-            @storage.should_receive(:increment)
+            expect(@storage).to receive(:fetch).and_return(Throttling.limits['foo']['limit'] - 1)
+            expect(@storage).to receive(:increment)
             @t.send(check_method, valid_value)
           end
 
           it 'should not increase hit counter when values equals to limit' do
-            @storage.should_receive(:fetch).and_return(Throttling.limits['foo']['limit'])
-            @storage.should_not_receive(:increment)
+            expect(@storage).to receive(:fetch).and_return(Throttling.limits['foo']['limit'])
+            expect(@storage).not_to receive(:increment)
             @t.send(check_method, valid_value)
           end
 
           it 'should not increase hit counter when values equals to limit + 1' do
-            @storage.should_receive(:fetch).and_return(Throttling.limits['foo']['limit'] + 1)
-            @storage.should_not_receive(:increment)
+            expect(@storage).to receive(:fetch).and_return(Throttling.limits['foo']['limit'] + 1)
+            expect(@storage).not_to receive(:increment)
             @t.send(check_method, valid_value)
           end
 
           it 'should allow exactly limit actions' do
-            5.times { @t.send(check_method, valid_value).should be_true }
-            @storage.should_not_receive(:increment)
-            @t.send(check_method, valid_value).should be_false
+            5.times { expect(@t.send(check_method, valid_value)).to be_true }
+            expect(@storage).not_to receive(:increment)
+            expect(@t.send(check_method, valid_value)).to be_false
           end
         end
       end
@@ -88,25 +88,25 @@ describe Throttling do
     end
 
     it 'should return false if at least one limit is reached' do
-      @storage.should_receive(:fetch).and_return(1, 100)
-      Throttling.for('foo').check_ip('127.0.0.1').should be_false
+      expect(@storage).to receive(:fetch).and_return(1, 100)
+      expect(Throttling.for('foo').check_ip('127.0.0.1')).to be_false
     end
 
     it 'should return true if none limits reached' do
-      @storage.should_receive(:fetch).and_return(1, 2)
-      Throttling.for('foo').check_ip('127.0.0.1').should be_true
+      expect(@storage).to receive(:fetch).and_return(1, 2)
+      expect(Throttling.for('foo').check_ip('127.0.0.1')).to be_true
     end
 
     it 'should sort limits by period' do
-      @storage.should_receive(:fetch).ordered.with(/\:one\:/, anything).and_return(0)
-      @storage.should_receive(:fetch).ordered.with(/\:two\:/, anything).and_return(0)
-      Throttling.for('foo').check_ip('127.0.0.1').should be_true
+      expect(@storage).to receive(:fetch).ordered.with(/\:one\:/, anything).and_return(0)
+      expect(@storage).to receive(:fetch).ordered.with(/\:two\:/, anything).and_return(0)
+      expect(Throttling.for('foo').check_ip('127.0.0.1')).to be_true
     end
 
     it 'should return as soon as limit reached' do
-      @storage.should_receive(:fetch).ordered.with(/\:one\:/, anything).and_return(10)
-      @storage.should_not_receive(:fetch).with(/\:two\:/)
-      Throttling.for('foo').check_ip('127.0.0.1').should be_false
+      expect(@storage).to receive(:fetch).ordered.with(/\:one\:/, anything).and_return(10)
+      expect(@storage).not_to receive(:fetch).with(/\:two\:/)
+      expect(Throttling.for('foo').check_ip('127.0.0.1')).to be_false
     end
   end
 
@@ -116,41 +116,41 @@ describe Throttling do
     end
 
     it 'should return value when limit is not reached' do
-      @storage.should_receive(:fetch).and_return(0)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 10
-      @storage.should_receive(:fetch).and_return(4)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 10
+      expect(@storage).to receive(:fetch).and_return(0)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(10)
+      expect(@storage).to receive(:fetch).and_return(4)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(10)
 
-      @storage.should_receive(:fetch).and_return(5)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 15
-      @storage.should_receive(:fetch).and_return(14)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 15
+      expect(@storage).to receive(:fetch).and_return(5)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(15)
+      expect(@storage).to receive(:fetch).and_return(14)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(15)
 
-      @storage.should_receive(:fetch).and_return(15)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 20
-      @storage.should_receive(:fetch).and_return(99)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 20
+      expect(@storage).to receive(:fetch).and_return(15)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(20)
+      expect(@storage).to receive(:fetch).and_return(99)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(20)
 
-      @storage.should_receive(:fetch).and_return(100)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 25
-      @storage.should_receive(:fetch).and_return(1000)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should == 25
+      expect(@storage).to receive(:fetch).and_return(100)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(25)
+      expect(@storage).to receive(:fetch).and_return(1000)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to eq(25)
     end
 
     it 'should increase hit counter' do
-      @storage.should_receive(:fetch).and_return(4)
-      @storage.should_receive(:increment)
+      expect(@storage).to receive(:fetch).and_return(4)
+      expect(@storage).to receive(:increment)
       Throttling.for('request_priority').check_ip('127.0.0.1')
 
-      @storage.should_receive(:fetch).and_return(1000)
-      @storage.should_receive(:increment)
+      expect(@storage).to receive(:fetch).and_return(1000)
+      expect(@storage).to receive(:increment)
       Throttling.for('request_priority').check_ip('127.0.0.1')
     end
 
     it 'should return false when highest limit reached' do
       Throttling.limits['request_priority'].delete('default_value')
-      @storage.should_receive(:fetch).and_return(1000)
-      Throttling.for('request_priority').check_ip('127.0.0.1').should be_false
+      expect(@storage).to receive(:fetch).and_return(1000)
+      expect(Throttling.for('request_priority').check_ip('127.0.0.1')).to be_false
     end
   end
 
@@ -165,7 +165,7 @@ describe Throttling do
         Timecop.freeze(Time.at(@timestamp)) do
           Throttling.for('foo').check_ip('127.0.0.1')
         end
-        @storage.values.keys.first.should == 'throttle:foo:ip:127.0.0.1:global:15442'
+        expect(@storage.values.keys.first).to eq('throttle:foo:ip:127.0.0.1:global:15442')
       end
     end
 
@@ -174,7 +174,7 @@ describe Throttling do
         Timecop.freeze(Time.at(@timestamp)) do
           Throttling.for('foo').check_ip('127.0.0.1')
         end
-        @storage.values.values.first[:expires_in].should == 13631
+        expect(@storage.values.values.first[:expires_in]).to eq(13631)
       end
     end
   end
